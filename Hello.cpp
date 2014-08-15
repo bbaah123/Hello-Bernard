e257d8d61b0e2188915060700de845ceeb6b2985
bbaah123@gmail.com <bbaah123@gmail.com>
2014-08-14 20:50:00 -0400

    Added Hello World program

---
+++ Hello.cpp
@@ -0,0 +1,14 @@
+// Hello.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello World" << endl;
+	system("pause");
+	return 0;
+}
+
