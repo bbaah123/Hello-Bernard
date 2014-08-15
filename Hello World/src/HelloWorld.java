5da3718d6abffc4affa3b8f461a6ebc2511ceb47
bbaah123@gmail.com <bbaah123@gmail.com>
2014-08-14 21:04:17 -0400

    Added HelloWorld.java

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld 
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		
+		System.out.println("\nI love Java Programming!");
+	}
+
+}
